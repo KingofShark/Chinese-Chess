592b34533a2686995ad4f7b13aa7e4fbb9c467dc
Mr-1504 <truongvan.minh1504@gmail.com>
2024-04-08 17:06:36 +0700

    add: notice label

--- src/chesspiece/StaticPieces.java
+++ src/chesspiece/StaticPieces.java
@@ -3,11 +3,14 @@ package chesspiece;
 import game.Check;
 import game.ChessBoard;
 import game.CountDown;
+import image.NewImage;
 import menu.CloseButton;
 import menu.Setting;
 import sound.SoundEffect;
 
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicSliderUI;
+import java.awt.*;
 import java.util.Random;
 import java.util.Vector;
 
@@ -16,6 +19,8 @@ import static chesspiece.Piece.*;
 public class StaticPieces {
     private static int turn, minute, second;
     private static int first;
+    private static final JLabel notice_1 = new JLabel();
+    private static final JLabel notice_2 = new JLabel();
     private static final CountDown clock_1 = new CountDown("Đen");
     private static final CountDown clock_2 = new CountDown("Đỏ");
     private static final CloseButton closeButton = new CloseButton();
@@ -67,63 +72,47 @@ public class StaticPieces {
         for(ChessPiece piece : StaticPieces.pieces)
             StaticPieces.chessBoard.add(piece);
     }
-
     public static Vector<ChessPiece> getPieces() {
         return pieces;
     }
-
     public static SoundEffect getSoundEffect() {
         return soundEffect;
     }
-
     public static Setting getSetting() {
         return setting;
     }
-
-    public static void setNewSetting() {
-        StaticPieces.setting = new Setting();
-    }
-
+    public static void setNewSetting() {StaticPieces.setting = new Setting();}
     public static CountDown getClock_1() {
         return StaticPieces.clock_1;
     }
-
     public static CountDown getClock_2() {
         return StaticPieces.clock_2;
     }
-
     public static int getTurn() {
         return turn;
     }
-
     public static void setTurn(int turn) {
         StaticPieces.turn = turn;
     }
-
     public static void setFirst(int first) {
         StaticPieces.first = first;
     }
-
     public static int getFirst() {
         return first;
     }
-
     public static ChessBoard getChessBoardPanel() {
         return StaticPieces.chessBoard;
     }
-    public static int getMinute() {
-        return minute;
-    }
-    public static void setMinute(int minute) {
-        StaticPieces.minute = minute;
-    }
+    public static int getMinute() {return StaticPieces.minute;}
+    public static void setMinute(int minute){StaticPieces.minute = minute;}
     public static int getSecond() {
         return second;
     }
-
     public static void setSecond(int second) {
         StaticPieces.second = second;
     }
+    public static JLabel getNotice_1(){return StaticPieces.notice_1;}
+    public static JLabel getNotice_2(){return StaticPieces.notice_2;}
     public static void setNew(JButton start){
         StaticPieces.setCheck();
         StaticPieces.setFirst(2);
@@ -135,5 +124,32 @@ public class StaticPieces {
         }
         StaticPieces.setNewSetting();
         StaticPieces.getChessBoardPanel().setNew(start);
+    }
+    public static void changeImage(String status, int type){
+        ImageIcon image = new ImageIcon(System.getProperty("user.dir") + "/src/image/effect/" + status +  ".png");
+        image = new NewImage().resizeImage(image, 200, 106);
+        if(type == 1) {
+            StaticPieces.getNotice_1().setIcon(image);
+            StaticPieces.getNotice_1().setVisible(true);
+            StaticPieces.getNotice_2().setVisible(false);
+        }else if(type == 2){
+            StaticPieces.getNotice_2().setIcon(image);
+            StaticPieces.getNotice_2().setVisible(true);
+            StaticPieces.getNotice_1().setVisible(false);
+        }else{
+            StaticPieces.getNotice_2().setVisible(false);
+            StaticPieces.getNotice_1().setVisible(false);
+        }
+    }
+    public static class CustomSliderUI extends BasicSliderUI {
+        public CustomSliderUI(JSlider b) {
+            super(b);
+        }
+        @Override
+        protected void installDefaults(JSlider slider) {
+            super.installDefaults(slider);
+            slider.setBackground(Color.WHITE);
+            slider.setForeground(Color.BLUE);
+        }
     }
 }
