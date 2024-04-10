47527f2e71a54dd781b1ec23cc8c1401f0115a35
Mr-1504 <truongvan.minh1504@gmail.com>
2024-04-10 10:06:37 +0700

    add: lastmove

--- src/game/ChessBoard.java
+++ src/game/ChessBoard.java
@@ -1,59 +1,78 @@
 package game;
 
-import chesspiece.*;
+import chesspiece.ChessPiece;
+import chesspiece.Piece;
+import chesspiece.StaticPieces;
+import file.IOFile;
 import image.NewImage;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Line2D;
+import java.util.Vector;
 
 public class ChessBoard extends JPanel implements Piece {
     private Boolean pause;
     private final JLabel timer_2;
     private final JLabel timer_1;
+    private final JButton back;
+
     public ChessBoard() {
-        ImageIcon imageIcon = new ImageIcon(System.getProperty("user.dir") + "/src/image/effect/ss.png");
-        imageIcon = new NewImage().resizeImage(imageIcon, 200, 106);
         StaticPieces.getNotice_1().setSize(200, 106);
         StaticPieces.getNotice_2().setSize(200, 106);
-        StaticPieces.getNotice_1().setIcon(imageIcon);
-        StaticPieces.getNotice_1().setLocation(11 * CELL_SIZE - CELL_SIZE/2, 3 * CELL_SIZE - CELL_SIZE/2);
-        StaticPieces.getNotice_2().setIcon(imageIcon);
-        StaticPieces.getNotice_2().setLocation(11 * CELL_SIZE - CELL_SIZE/2, 7 * CELL_SIZE);
+        StaticPieces.getNotice_1().setLocation(11 * CELL_SIZE - CELL_SIZE / 2, 3 * CELL_SIZE - CELL_SIZE / 2);
+        StaticPieces.getNotice_2().setLocation(11 * CELL_SIZE - CELL_SIZE / 2, 7 * CELL_SIZE);
+        StaticPieces.changeImage("ss", 1);
+        StaticPieces.changeImage("ss", 2);
+        StaticPieces.getNotice_1().setVisible(true);
+        StaticPieces.getNotice_2().setVisible(true);
         this.add(StaticPieces.getNotice_2());
         this.add(StaticPieces.getNotice_1());
 
+        this.back = new JButton();
+        this.back.setSize(35, 35);
+        ImageIcon image = new ImageIcon(System.getProperty("user.dir") + "/src/image/menu/back.png");
+        image = new NewImage().resizeImage(image, 35, 35);
+        this.back.setIcon(image);
+        this.back.setLocation(CELL_SIZE * 13, CELL_SIZE * 5 + CELL_SIZE / 4);
+        this.back.setRolloverEnabled(false);
+        this.back.setBorderPainted(false);
+        this.back.setContentAreaFilled(false);
         this.timer_2 = new JLabel();
         this.timer_1 = new JLabel();
         this.timer_2.setLocation(CELL_SIZE * 11, CELL_SIZE * 6 + CELL_SIZE / 4);
         this.timer_1.setLocation(CELL_SIZE * 11, CELL_SIZE * 4 + CELL_SIZE / 4);
 
         this.pause = true;
+        this.setBackground(new Color(238, 234, 234));
         this.setSize(_width_, _height_);
+        this.add(this.back);
         this.add(this.timer_2);
         this.add(this.timer_1);
         StaticPieces.getClock_1().start(timer_1);
         StaticPieces.getClock_2().start(timer_2);
         this.setLayout(null);
-        this.setBackground(new Color(205, 160, 70));
     }
 
-    public Boolean getPause() {
-        return pause;
-    }
-    public void setNew(JButton start){
+
+    public void setNew(JButton start) {
         this.setTime(0, StaticPieces.getMinute(), StaticPieces.getSecond());
         this.setTime(1, StaticPieces.getMinute(), StaticPieces.getSecond());
         this.setButton(start);
+        this.backLastMove(start);
         this.play(start);
         this.setVisible(true);
     }
-    public void removePieces(){
-        for(ChessPiece piece : StaticPieces.getPieces())
+
+    public void removePieces() {
+        for (ChessPiece piece : StaticPieces.getPieces())
             piece.resetDefauft();
         this.removeAll();
         this.add(this.timer_1);
         this.add(this.timer_2);
+        this.add(this.back);
+        this.add(StaticPieces.getNotice_1());
+        this.add(StaticPieces.getNotice_2());
         this.pause = true;
     }
 
@@ -61,30 +80,30 @@ public class ChessBoard extends JPanel implements Piece {
         StaticPieces.getCloseButton().setClose(this);
         StaticPieces.getCloseButton().setHide(this);
         button.addActionListener(e -> {
-            if(StaticPieces.getTurn() == -1)
+            if (StaticPieces.getTurn() == -1)
                 return;
 
             if (StaticPieces.getFirst() == 2) {
-                JOptionPane.showMessageDialog(null, (StaticPieces.getTurn() % 2 == 1) ? "Đen đi trước" : "Đỏ đi trước");
+                JOptionPane.showMessageDialog(null, (StaticPieces.getTurn() % 2 == 1) ? "Đen đi trước" : "Đỏ đi trước", "người đi trước", JOptionPane.INFORMATION_MESSAGE);
                 ImageIcon imageIcon = new ImageIcon(System.getProperty("user.dir") + "/src/image/start.png");
                 imageIcon = new NewImage().resizeImage(imageIcon, 90, 32);
                 button.setIcon(imageIcon);
                 if (StaticPieces.getTurn() % 2 == Piece.BLACK) {
                     StaticPieces.getClock_1().resume();
-                    StaticPieces.changeImage("wait", 1);
+                    StaticPieces.changeImage("wait", 2);
                 } else {
                     StaticPieces.getClock_2().resume();
-                    StaticPieces.changeImage("wait", 2);
+                    StaticPieces.changeImage("wait", 1);
                 }
             }
             StaticPieces.setFirst(StaticPieces.getTurn() % 2);
-            if (pause) {
+            if (this.pause) {
                 if (StaticPieces.getFirst() == Piece.BLACK) {
                     StaticPieces.getClock_1().resume();
                     StaticPieces.changeImage("wait", 1);
                 } else {
                     StaticPieces.getClock_2().resume();
-                    StaticPieces.changeImage("wait", 1);
+                    StaticPieces.changeImage("wait", 2);
                 }
                 ImageIcon imageIcon = new ImageIcon(System.getProperty("user.dir") + "/src/image/stop.png");
                 imageIcon = new NewImage().resizeImage(imageIcon, 90, 32);
@@ -115,15 +134,14 @@ public class ChessBoard extends JPanel implements Piece {
         this.add(button);
     }
 
-
     public void setTime(int type, int minute, int second) {
-        if(type == 0) {
+        if (type == 0) {
             this.timer_1.setSize(100, 30);
             this.timer_1.setFont(new Font("Arial", Font.BOLD, 30));
             String _minute = minute > 9 ? "" + minute : "0" + minute;
             String _second = second > 9 ? "" + second : "0" + second;
             this.timer_1.setText(_minute + " : " + _second);
-        }else{
+        } else {
             this.timer_2.setSize(100, 30);
             this.timer_2.setFont(new Font("Arial", Font.BOLD, 30));
             String _minute = minute > 9 ? "" + minute : "0" + minute;
@@ -138,27 +156,8 @@ public class ChessBoard extends JPanel implements Piece {
         drawBoard(g);
     }
 
-    public void drawBoard(Graphics g) {
+    private void drawBoard(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        // Khung bàn cờ
-
-        g.setColor(new Color(152, 93, 58));
-        int row, col;
-        for (row = 0; row < CELL_SIZE / 2; row++)
-            for (col = 0; col < CELL_SIZE; col++)
-                g.fillRect(row * CELL_SIZE, col * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-        for (row = CELL_SIZE / 2; row < CELL_SIZE; row++)
-            for (col = 0; col < CELL_SIZE / 2; col++)
-                g.fillRect(row * CELL_SIZE, col * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-        for (row = CELL_SIZE / 2 + 1; row < CELL_SIZE + 1; row++)
-            for (col = 0; col < CELL_SIZE; col++)
-                g.fillRect(row * CELL_SIZE, col * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-        for (row = 0; row < CELL_SIZE; row++) {
-            for (col = CELL_SIZE / 2 + 1; col < CELL_SIZE; col++) {
-                g.fillRect(row * CELL_SIZE, col * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-            }
-        }
-
         g.setColor(new Color(216, 83, 39));
 
         Stroke stroke = new BasicStroke(5.0f);
@@ -196,6 +195,45 @@ public class ChessBoard extends JPanel implements Piece {
         g2d.draw(line);
         line = new Line2D.Double(5 * CELL_SIZE, 6 * CELL_SIZE, 9 * CELL_SIZE, 5 * CELL_SIZE - 1);
         g2d.draw(line);
+
+        //
+        line = new Line2D.Double((double) CELL_SIZE / 2, (double) CELL_SIZE / 2, 9 * CELL_SIZE + (double) CELL_SIZE / 2, (double) CELL_SIZE / 2);
+        g2d.draw(line);
+        line = new Line2D.Double((double) CELL_SIZE / 2, 10 * CELL_SIZE + (double) CELL_SIZE / 2, 9 * CELL_SIZE + (double) CELL_SIZE / 2, 10 * CELL_SIZE + (double) CELL_SIZE / 2);
+        g2d.draw(line);
+        line = new Line2D.Double((double) CELL_SIZE / 2, (double) CELL_SIZE / 2, (double) CELL_SIZE / 2, 10 * CELL_SIZE + (double) CELL_SIZE / 2);
+        g2d.draw(line);
+        line = new Line2D.Double(9 * CELL_SIZE + (double) CELL_SIZE / 2, (double) CELL_SIZE / 2, 9 * CELL_SIZE + (double) CELL_SIZE / 2, 10 * CELL_SIZE + (double) CELL_SIZE / 2);
+        g2d.draw(line);
+    }
+
+    private void backLastMove(JButton button) {
+        this.back.addActionListener(e -> {
+            if (!IOFile.checkFile(System.getProperty("user.dir") + "/src/file/lastmove.txt"))
+                return;
+            this.pause = true;
+            StaticPieces.getEvent().hideButton();
+            StaticPieces.getClock_1().stop();
+            StaticPieces.getClock_2().stop();
+            StaticPieces.changeImage("", 0);
+            ImageIcon imageIcon = new ImageIcon(System.getProperty("user.dir") + "/src/image/start.png");
+            imageIcon = new NewImage().resizeImage(imageIcon, 90, 32);
+            button.setIcon(imageIcon);
+            Vector<Integer> last = IOFile.loadLastMove();
+            int locateX = last.elementAt(1);
+            int locateY = last.elementAt(2);
+            int x = StaticPieces.getPieces().elementAt(last.firstElement()).getLocateX();
+            int y = StaticPieces.getPieces().elementAt(last.firstElement()).getLocateY();
+            StaticPieces.getCheck().setPiece(x, y, last.elementAt(3));
+            StaticPieces.getPieces().elementAt(last.firstElement()).setLocate(locateX, locateY);
+            StaticPieces.getCheck().setPiece(last.elementAt(1), last.elementAt(2), last.firstElement());
+            StaticPieces.setTurn(last.lastElement());
+            if (last.elementAt(3) != -1)
+                StaticPieces.getPieces().elementAt(last.elementAt(3)).setVisible(true);
+        });
+    }
+    public Boolean getPause() {
+        return pause;
     }
 }
 
