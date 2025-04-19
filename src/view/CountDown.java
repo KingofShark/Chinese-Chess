2f2ba126c55d5a3c5c2387450b67a6dc7be8b93b
Mr-1504 <truongvan.minh1504@gmail.com>
2025-04-20 01:08:29 +0700

    Update CountDown.java

--- src/view/CountDown.java
+++ src/view/CountDown.java
@@ -7,21 +7,23 @@ import java.util.TimerTask;
 
 public class CountDown {
     private int minute, second;
-    private final String name;
     private final Timer timer;
     private Boolean pause;
     private Boolean fullTime;
-    public CountDown(String name){
+    private FullTimeListener listener;
+    private JLabel label;
+    public CountDown(){
         this.pause = true;
-        this.name = name;
         this.fullTime = false;
         this.timer = new Timer();
     }
-    public void start(JLabel label){
+    public void start(JLabel label, final FullTimeListener listener){
+        this.listener = listener;
+        this.label = label;
         this.timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                updateTime(label);
+                updateTime();
             }
         }, 0, 1000);
     }
@@ -31,11 +33,11 @@ public class CountDown {
         this.fullTime = false;
     }
 
-    private void updateTime(JLabel label){
+    private void updateTime(){
         if(!pause && !fullTime&& !StaticPieces.getSetting().getStatus()){
             if (this.minute == 0 && this.second == 0) {
                 this.fullTime = true;
-                JOptionPane.showMessageDialog(null, "Hết giờ\n" + name + " thua");
+                listener.onFullTime();
             } else {
                 if (this.second == 0) {
                     this.minute--;
@@ -49,9 +51,27 @@ public class CountDown {
             }
         }
     }
+
+    public void setFullTime(Boolean fullTime) {
+        this.fullTime = fullTime;
+    }
+
     public Boolean getFullTime() { return fullTime;}
-    public void stop(){ this.pause = true;}
-    public void resume(){ this.pause = false;}
+    public void stop(){
+        this.pause = true;
+        this.minute = StaticPieces.getMinute();
+        this.second = StaticPieces.getSecond();
+        String temp1 = (this.minute < 10) ? "0" : "";
+        String temp2 = (this.second < 10) ? "0" : "";
+        label.setText(temp1 + this.minute + " : " + temp2 + this.second);
+    }
+    public void resume(){
+        this.pause = false;
+    }
     public int getMinute() {return minute;}
     public int getSecond() {return second;}
+
+    public interface FullTimeListener {
+        void onFullTime();
+    }
 }
